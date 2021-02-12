4101d21eb2f8c0e7b5f7049c111ab603bbf3fa3a
tottisilva <tottisilva19@gmail.com>
2021-02-12 20:08:05 +0000

    first commit

---
+++ src/components/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It's done with TraversyMedia tutorial</p>
+    <p>Check he's Github repository https://github.com/bradtraversy</p>
+  </div>
+</template>
